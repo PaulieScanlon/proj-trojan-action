9cc8316a16e307729908e8222a7e51ae850d15fd
Paul Scanlon <pauliescanlon@gmail.com>
2024-06-24 19:53:28 +0100

    refactor: add SLACK_WEBHOOK_URL to action env

--- src/index.js
+++ src/index.js
@@ -3,46 +3,41 @@ dotenv.config();
 
 const init = async () => {
   const bytes = parseInt(process.env.ARTIFACT_SIZE_BYTES, 10);
-  const kilobytes = bytes / 1024;
+  const kilobytes = (bytes / 1024).toFixed(2);
   const date = new Intl.DateTimeFormat('en-US', {
     dateStyle: 'full',
     timeStyle: 'short',
   }).format(new Date());
 
-  console.log('bytes: ', bytes);
-  console.log('kilobytes: ', kilobytes);
-  console.log('date: ', date);
-  console.log('process.env.SLACK_WEBHOOK_URL: ', process.env.SLACK_WEBHOOK_URL);
-
   try {
-    // fetch(process.env.SLACK_WEBHOOK_URL, {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: JSON.stringify({
-    //     blocks: [
-    //       {
-    //         type: 'header',
-    //         text: {
-    //           type: 'plain_text',
-    //           text: 'RDS Prod to Neon',
-    //           emoji: true,
-    //         },
-    //       },
-    //       {
-    //         type: 'divider',
-    //       },
-    //       {
-    //         type: 'section',
-    //         text: {
-    //           type: 'plain_text',
-    //           text: `${kilobytes} KB restored: ${date}`,
-    //         },
-    //       },
-    //     ],
-    //   }),
-    // });
+    fetch(process.env.SLACK_WEBHOOK_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        blocks: [
+          {
+            type: 'header',
+            text: {
+              type: 'plain_text',
+              text: 'RDS Prod to Neon',
+              emoji: true,
+            },
+          },
+          {
+            type: 'divider',
+          },
+          {
+            type: 'section',
+            text: {
+              type: 'plain_text',
+              text: `${kilobytes} KB restored: ${date}`,
+            },
+          },
+        ],
+      }),
+    });
   } catch (error) {
     console.error(error);
   }
