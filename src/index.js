80ca1166ef455ab4b0b942c201f9d7f9bf8c2c2a
Paul Scanlon <pauliescanlon@gmail.com>
2024-06-24 19:39:55 +0100

    feat: add fetch to post slack webhook

--- src/index.js
+++ src/index.js
@@ -2,12 +2,50 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const init = async () => {
-  console.log('init');
-  const artifactSizeInBytes = parseInt(process.env.ARTIFACT_SIZE_BYTES, 10);
-  const artifactSizeInKB = artifactSizeInBytes / 1024;
+  const bytes = parseInt(process.env.ARTIFACT_SIZE_BYTES, 10);
+  const kilobytes = bytes / 1024;
+  const date = new Intl.DateTimeFormat('en-US', {
+    dateStyle: 'full',
+    timeStyle: 'short',
+  }).format(new Date());
 
-  console.log(`artifactSizeInBytes: ${artifactSizeInKB} bytes`);
-  console.log(`artifactSizeInKB: ${artifactSizeInKB} KB`);
+  console.log('bytes: ', bytes);
+  console.log('kilobytes: ', kilobytes);
+  console.log('date: ', date);
+  console.log('process.env.SLACK_WEBHOOK_URL: ', process.env.SLACK_WEBHOOK_URL);
+
+  try {
+    // fetch(process.env.SLACK_WEBHOOK_URL, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({
+    //     blocks: [
+    //       {
+    //         type: 'header',
+    //         text: {
+    //           type: 'plain_text',
+    //           text: 'RDS Prod to Neon',
+    //           emoji: true,
+    //         },
+    //       },
+    //       {
+    //         type: 'divider',
+    //       },
+    //       {
+    //         type: 'section',
+    //         text: {
+    //           type: 'plain_text',
+    //           text: `${kilobytes} KB restored: ${date}`,
+    //         },
+    //       },
+    //     ],
+    //   }),
+    // });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 init();
