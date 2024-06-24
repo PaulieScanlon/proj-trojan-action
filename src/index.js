01e63764388b17bcf548df9fdf871f305d5442cd
Paul Scanlon <pauliescanlon@gmail.com>
2024-06-24 18:13:47 +0100

    feat: add artifact size

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const init = async () => {
   console.log('init');
+  const artifactSizeInBytes = parseInt(process.env.ARTIFACT_SIZE_BYTES, 10);
+  const artifactSizeInKB = artifactSizeInBytes / 1024;
+
+  console.log(`artifactSizeInBytes: ${artifactSizeInKB} bytes`);
+  console.log(`artifactSizeInKB: ${artifactSizeInKB} KB`);
 };
 
 init();
