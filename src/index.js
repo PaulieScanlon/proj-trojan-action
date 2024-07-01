f92f15ade06018591e3c50770267854747aa9139
Paul Scanlon <pauliescanlon@gmail.com>
2024-07-01 16:19:34 +0100

    refactor: include better conditions in index.js

--- src/index.js
+++ src/index.js
@@ -2,71 +2,80 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const init = async () => {
-  const bytes = parseInt(process.env.DATABASE_SIZE_IN_BYTES, 10);
-  const jobStatus = process.env.JOB_STATUS;
-
-  console.log('jobStatus: ', jobStatus);
-
-  const kilobytes = (bytes / 1024).toFixed(2);
-  const megabytes = (bytes / (1024 * 1024)).toFixed(2);
-  const gigabytes = (bytes / (1024 * 1024 * 1024)).toFixed(2);
-  const gibibytes = (bytes / (1024 * 1024 * 1024)).toFixed(2);
+  const jobStatus = process.env.JOB_STATUS || 'unknown';
 
   const date = new Intl.DateTimeFormat('en-US', {
     dateStyle: 'full',
     timeStyle: 'short',
   }).format(new Date());
 
-  const failure = [
-    {
-      type: 'header',
-      text: {
-        type: 'plain_text',
-        text: '⚠️ A Neon Twin failed',
-        emoji: true,
-      },
-    },
-    {
-      type: 'divider',
-    },
-    {
-      type: 'section',
-      text: {
-        type: 'plain_text',
-        text: `Latest Twin failed: ${date}`,
-      },
-    },
-  ];
+  const bytes = parseInt(process.env.DATABASE_SIZE_IN_BYTES, 10) || 0;
+  const kilobytes = (bytes / 1024).toFixed(2);
+  const megabytes = (bytes / (1024 * 1024)).toFixed(2);
+  const gigabytes = (bytes / (1024 * 1024 * 1024)).toFixed(2);
+  const gibibytes = (bytes / (1024 * 1024 * 1024)).toFixed(2);
 
-  const success = [
-    {
-      type: 'header',
-      text: {
-        type: 'plain_text',
-        text: '☝️ A new Neon Twin is available!',
-        emoji: true,
-      },
-    },
-    {
-      type: 'divider',
-    },
-    {
-      type: 'section',
-      text: {
-        type: 'plain_text',
-        text: `Latest Twin created: ${date}`,
-      },
-    },
-    {
-      type: 'context',
-      elements: [
-        {
-          type: 'mrkdwn',
-          text: `• Kilobytes: ${kilobytes} KB\n• Megabytes: ${megabytes} MB\n• Gigabytes: ${gigabytes} GB\n• Gibibytes: ${gibibytes} GiB`,
+  const messages = {
+    success: [
+      {
+        type: 'header',
+        text: {
+          type: 'plain_text',
+          text: '☝️ A new Neon Twin is available!',
+          emoji: true,
         },
-      ],
-    },
-  ];
+      },
+      {
+        type: 'divider',
+      },
+      {
+        type: 'section',
+        text: {
+          type: 'plain_text',
+          text: `Latest Twin created: ${date}`,
+        },
+      },
+      {
+        type: 'context',
+        elements: [
+          {
+            type: 'mrkdwn',
+            text: `• Kilobytes: ${kilobytes} KB\n• Megabytes: ${megabytes} MB\n• Gigabytes: ${gigabytes} GB\n• Gibibytes: ${gibibytes} GiB`,
+          },
+        ],
+      },
+    ],
+    failure: [
+      {
+        type: 'header',
+        text: {
+          type: 'plain_text',
+          text: '⚠️ A Neon Twin failed',
+          emoji: true,
+        },
+      },
+      {
+        type: 'divider',
+      },
+      {
+        type: 'section',
+        text: {
+          type: 'plain_text',
+          text: `Latest Twin failed: ${date}`,
+        },
+      },
+    ],
+    unknown: [
+      {
+        type: 'header',
+        text: {
+          type: 'plain_text',
+          text: '❗ Job status unknown',
+          emoji: true,
+        },
+      },
+    ],
+  };
 
   try {
     fetch(process.env.SLACK_WEBHOOK_URL, {
@@ -75,7 +84,7 @@ const init = async () => {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        blocks: jobStatus === 'failure' ? failure : success,
+        blocks: messages[jobStatus],
       }),
     });
   } catch (error) {
