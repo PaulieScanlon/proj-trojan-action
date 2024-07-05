9a916e32f1b93475018c6f095d43c1b0a92d34d5
Paul Scanlon <pauliescanlon@gmail.com>
2024-07-05 09:07:42 +0100

    feat: add start and end time to outputs

--- src/slack-success.js
+++ src/slack-success.js
@@ -1,15 +1,30 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+const formatDate = (dateString) => {
+  return new Date(dateString).toLocaleString('en-US', {
+    timeZone: 'UTC',
+    day: 'numeric',
+    month: 'long',
+    weekday: 'long',
+    year: 'numeric',
+  });
+};
+
 const init = async () => {
-  const date = new Intl.DateTimeFormat('en-US', {
-    dateStyle: 'full',
-    timeStyle: 'short',
-  }).format(new Date());
+  const date = new Date();
 
   const bytes = parseInt(process.env.DATABASE_SIZE, 10) || 0;
   const gigabytes = (bytes / (1024 * 1024 * 1024)).toFixed(2);
-  const name = process.env.DATABASE_NAME;
+  const name = process.env.DATABASE_NAME || '';
+
+  const start = process.env.JOB_START_TIME || new Date();
+  const end = process.env.JOB_END_TIME || new Date();
+  const duration = new Date(end) - new Date(start);
+
+  const hours = Math.floor(duration / (1000 * 60 * 60));
+  const minutes = Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((duration % (1000 * 60)) / 1000);
 
   try {
     fetch(process.env.SLACK_WEBHOOK_URL, {
@@ -34,7 +49,7 @@ const init = async () => {
             type: 'section',
             text: {
               type: 'plain_text',
-              text: `Latest Twin created: ${date}`,
+              text: `Latest Twin created: ${formatDate(date)}`,
             },
           },
           {
@@ -42,7 +57,9 @@ const init = async () => {
             elements: [
               {
                 type: 'mrkdwn',
-                text: `• ${gigabytes} GB twinned from ${name}`,
+                text: `• ${gigabytes} GB twinned from ${name}\n• Start: ${formatDate(start)}\n• End: ${formatDate(
+                  end
+                )}\n• Duration: ${hours} hours, ${minutes} minutes, ${seconds} seconds`,
               },
             ],
           },
