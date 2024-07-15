9bb69d3aaeda549dbf818f4786484997d29238b7
Paul Scanlon <pauliescanlon@gmail.com>
2024-07-15 18:05:09 +0100

    chore: update schema

--- schema.sql
+++ schema.sql
@@ -1,7 +1,18 @@
-CREATE TABLE users (
-  id          SERIAL PRIMARY KEY,
-  first_name  VARCHAR(255) NOT NULL,
-  last_name   VARCHAR(255) NOT NULL,
-  email       VARCHAR(255) NOT NULL UNIQUE,
-  country     VARCHAR(255) NOT NULL
+CREATE TABLE test_table (
+    id SERIAL PRIMARY KEY,
+    some_data TEXT
 );
+
+INSERT INTO test_table (some_data)
+SELECT 'Some data ' || generate_series
+FROM generate_series(1, 100000000);
+
+
+-- In MB
+SELECT pg_size_pretty(pg_database_size('neondb'));
+
+-- In GiB
+SELECT pg_size_pretty(pg_database_size('neondb') / (1024 * 1024 * 1024)) AS size_in_gib;
+
+
+SELECT pg_size_pretty(pg_total_relation_size('test_table')) AS table_size;
