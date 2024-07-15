3c3dfd3860d37a30ed255327c0085f859ba53b8a
Paul Scanlon <pauliescanlon@gmail.com>
2024-07-15 21:08:17 +0100

    refactor: rename actions

--- schema.sql
+++ schema.sql
@@ -1,18 +1,2 @@
-CREATE TABLE test_table (
-    id SERIAL PRIMARY KEY,
-    some_data TEXT
-);
-
-INSERT INTO test_table (some_data)
-SELECT 'Some data ' || generate_series
-FROM generate_series(1, 100000000);
-
-
 -- In MB
-SELECT pg_size_pretty(pg_database_size('neondb'));
-
--- In GiB
-SELECT pg_size_pretty(pg_database_size('neondb') / (1024 * 1024 * 1024)) AS size_in_gib;
-
-
-SELECT pg_size_pretty(pg_total_relation_size('test_table')) AS table_size;
+SELECT pg_size_pretty(pg_database_size('postgres'));
